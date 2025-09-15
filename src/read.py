00ba0ff0c4a02fe145bf97dd4f3092476e6d386b
antonio <antoniolandin@proton.me>
2025-09-15 22:26:43 +0200

    feat: show error in json

--- src/read.py
+++ src/read.py
@@ -6,14 +6,18 @@ def read(FILE_PATH: str) -> dict:
 
     classes = {}
 
-    with open(FILE_PATH, 'r') as file:
+    with open(FILE_PATH, "r") as file:
         json_data = json.load(file)
 
         # go through the keys of the json file
         for group in json_data.keys():
             # create a list of classes for each subject
-            classes[group] = [
-                Lecture(**lecture, group=group) for lecture in json_data[group]
-            ]
+            try:
+                classes[group] = [
+                    Lecture(**lecture, group=group) for lecture in json_data[group]
+                ]
+            except Exception as e:
+                print(group)
+                raise e
 
     return classes
